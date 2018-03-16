a3bcdcc59b0f9d402aef04b4e420bee3279886cc
Araneta <arjunaraneta@gmail.com>
2018-03-16 09:48:44 +0800

    2018 Updates

---
+++ Conversions/problem13.r
@@ -0,0 +1,5 @@
+//Problem 13: Write a program that converts celsius to farenheit.
+celsius <- 10.0
+farenheit = (9.0/5.0) * as.numeric(celsius) + 32.0
+
+print(paste(celsius, "C equals to " ,farenheit))
