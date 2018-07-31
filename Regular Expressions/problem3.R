55f6da6b203c694aa29f0ff26bd1d3ab2d3bb137
Arjun Araneta <arjunaraneta@gmail.com>
2018-08-01 07:44:51 +0800

    2018 Updates

---
+++ Regular Expressions/problem3.R
@@ -0,0 +1,2 @@
+#Problem 3: Write a program that checks if string contains sample format date of (yyyy-mm-dd)
+print(grepl("([0-9]{4})-([0-9]{2})-([0-9]{2})", c("2018-01-02", "01-01-02"), perl=TRUE))
