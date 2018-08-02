6ae9ee29aeb6f67a531aeacddccfa0528d600037
Arjun Araneta <arjunaraneta@gmail.com>
2018-08-02 10:18:04 +0800

    2018 Updates

---
+++ Regular Expressions/problem14.R
@@ -0,0 +1,2 @@
+#Problem 14: Write a program that recognizes valid hex color value using Regular Expression.
+print(grepl("^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$", c("192.168.1.1", "1.1.1.1.1"), perl=TRUE))
