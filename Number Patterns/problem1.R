37704cf9611a6c7210dbe004d9f53218d50cbf8f
Arjun Araneta <arjunaraneta@gmail.com>
2018-11-23 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/problem1.R
@@ -0,0 +1,14 @@
+# Problem 1: Write a program to print the number pattern of ones and zeros using loop.
+# 11111
+# 11111
+# 11111
+# 11111
+# 11111
+
+for (y in 1:5){
+ for (x in 1:5) {
+  cat(paste("1"))
+ }
+ cat(paste("\n"))
+}
+
